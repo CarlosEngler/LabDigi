52d0ddd34db0c323b83c793cbbbfa9ba1d470298
CarlosEngler <Carlos.engler@usp.br>
2024-01-21 00:20:44 -0300

    finish description

--- Experiment 3/circuito_exp3_ativ2.v
+++ Experiment 3/circuito_exp3_ativ2.v
@@ -5,8 +5,11 @@
  * ------------------------------------------------------------------
  *  Descricao : Circuito PARCIAL do fluxo de dados da Atividade 2
  * 
- *     1) COMPLETAR DESCRICAO
- * 
+ *     1) circuito composto dos componentes contador 74163 e comparador 7485. 
+ *        As saídas do contador são conectadas às entradas do comparador.
+ *        A entrada "Chaves"  é conectada às entradas "A" do comparador e "D" do contador.
+ *        O circuito tem como output o valor da contagem do contador e as comparações realizadas pelo comparador.
+ *
  * ------------------------------------------------------------------
  *  Revisoes  :
  *      Data        Versao  Autor             Descricao
@@ -57,3 +60,35 @@ module circuito_exp3_ativ2 (clock, zera, carrega, conta, chaves,
     assign db_contagem = s_contagem;
 
  endmodule
+
+ module comparador_85 (ALBi, AGBi, AEBi, A, B, ALBo, AGBo, AEBo);
+
+    input[3:0] A, B;
+    input      ALBi, AGBi, AEBi;
+    output     ALBo, AGBo, AEBo;
+    wire[4:0]  CSL, CSG;
+
+    assign CSL  = ~A + B + ALBi;
+    assign ALBo = ~CSL[4];
+    assign CSG  = A + ~B + AGBi;
+    assign AGBo = ~CSG[4];
+    assign AEBo = ((A == B) && AEBi);
+
+endmodule /* comparador_85 */
+
+module contador_163 ( clock, clr, ld, ent, enp, D, Q, rco );
+    input clock, clr, ld, ent, enp;
+    input [3:0] D;
+    output reg [3:0] Q;
+    output reg rco;
+
+    always @ (posedge clock)
+        if (~clr)               Q <= 4'd0;
+        else if (~ld)           Q <= D;
+        else if (ent && enp)    Q <= Q + 1;
+        else                    Q <= Q;
+ 
+    always @ (Q or ent)
+        if (ent && (Q == 4'd15))   rco = 1;
+        else                       rco = 0;
+endmodule
