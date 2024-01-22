c56689839e9c704078461947218b8ce817ee84c5
CarlosEngler <Carlos.engler@usp.br>
2024-01-22 10:33:55 -0300

    adicionar desafio v0

--- Experiment 3/circuito_exp3_ativ2.v
+++ Experiment 3/circuito_exp3_ativ2.v
@@ -28,7 +28,7 @@ module circuito_exp3_ativ2 (clock, zera, carrega, conta, chaves,
     output       maior;
     output       igual;
     output       fim;
-    output [3:0] db_contagem;
+    output [6:0] db_contagem;
 
     wire   [3:0] s_contagem;  // sinal interno para interligacao dos componentes
 
@@ -56,39 +56,9 @@ module circuito_exp3_ativ2 (clock, zera, carrega, conta, chaves,
       .AEBo( igual )
     );
 
-    // saida de depuracao
-    assign db_contagem = s_contagem;
+    hexa7seg HEX0 (
+      .hexa(s_contagem),
+      .display(db_contagem)
+    );
 
- endmodule
-
- module comparador_85 (ALBi, AGBi, AEBi, A, B, ALBo, AGBo, AEBo);
-
-    input[3:0] A, B;
-    input      ALBi, AGBi, AEBi;
-    output     ALBo, AGBo, AEBo;
-    wire[4:0]  CSL, CSG;
-
-    assign CSL  = ~A + B + ALBi;
-    assign ALBo = ~CSL[4];
-    assign CSG  = A + ~B + AGBi;
-    assign AGBo = ~CSG[4];
-    assign AEBo = ((A == B) && AEBi);
-
-endmodule /* comparador_85 */
-
-module contador_163 ( clock, clr, ld, ent, enp, D, Q, rco );
-    input clock, clr, ld, ent, enp;
-    input [3:0] D;
-    output reg [3:0] Q;
-    output reg rco;
-
-    always @ (posedge clock)
-        if (~clr)               Q <= 4'd0;
-        else if (~ld)           Q <= D;
-        else if (ent && enp)    Q <= Q + 1;
-        else                    Q <= Q;
- 
-    always @ (Q or ent)
-        if (ent && (Q == 4'd15))   rco = 1;
-        else                       rco = 0;
 endmodule
