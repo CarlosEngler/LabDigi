eb6c0fff8b989669b72d0cceb395469a58599e5b
CarlosEngler <Carlos.engler@usp.br>
2024-02-17 17:33:50 -0300

    nova testbench saindo do forno
    Co-authored-by: JPBF100 <joao.bassetti@polijunior.com.br>

--- Experimento_6/testbenchs/fluxo_de_dados_tb.v
+++ Experimento_6/testbenchs/fluxo_de_dados_tb.v
@@ -21,71 +21,68 @@ module circuito_exp6_tb2;
 
     // Sinais para conectar com o DUT
     // valores iniciais para fins de simulacao (ModelSim)
-reg input_clock;
-reg input_zeraCR;
-reg input_zeraE;
-reg input_contaCR;
-reg input_contaE;
-reg input_limpaRC;
-reg input_registraRC;
-reg input_zeraLeds;
-reg input_registraLeds;
-reg input_contaT;
-reg [3:0] input_botoes;
-reg input_led_selector;
 
-wire jogada_correta;
-wire enderecoIgualRodada; 
-wire fimC;
-wire fimL;
-wire jogada_feita;
-wire db_tem_jogada;
-wire timeout;
-wire [3:0] db_contagem;
-wire [3:0] db_memoria;
-wire [3:0] db_rodada;
-wire [3:0] db_jogada;
-wire [3:0] leds;
+  reg clock_in;
+  reg zeraCR_in;
+  reg zeraE_in;
+  reg contaCR_in;
+  reg contaE_in;
+  reg limpaRC_in;
+  reg registraRC_in;
+  reg zeraLeds_in;
+  reg registraLeds_in;
+  reg contaT_in;
+  reg [3:0] botoes_in;
+  reg led_selector_in;
+  reg jogar;
+  wire jogada_correta_out;
+  wire enderecoIgualRodada_out;
+  wire fimC_out;
+  wire fimL_out;
+  wire jogada_feita_out;
+  wire db_tem_jogada_out;
+  wire timeout_out;
+  wire [3:0] db_contagem_out;
+  wire [3:0] db_memoria_out;
+  wire [3:0] db_jogada_out;
+  wire [3:0] db_rodada_out;
+  wire [3:0] leds_out;
 
 
+  reg [3:0] Eatual, Eprox;
 
     // Configuração do clock
     parameter clockPeriod = 20; // in ns, f=50MHz
-
-    // Identificacao do caso de teste
-    reg [31:0] caso = 0;
-    reg [31:0] rodada = 0;
-    reg [31:0] jogada = 0;
 
     // Gerador de clock
     always #((clockPeriod / 2)) clock_in = ~clock_in;
 
     // Instancia do DUT
-    exp6_fluxo_dados ffut(
-    .clock      (clock),
-    .botoes     (botoes),
-    .limpaRC         (w_limparRC),
-    .registraRC       (w_registraRC),
-    .zeraLeds       (w_zeraLeds),
-    .registraLeds   (w_registraLeds),
-    .contaCR    (w_contaCR),
-    .zeraCR (w_zeraCR),
-    .contaE (w_contaE),
-    .zeraE  (w_zeraE),
-    .enderecoIgualRodada    (w_enderecoIgualRodada),
-    .jogada_correta (w_jogada_correta),
-    .fimC   ( ),
-    .fimL   (w_fimL),
-	  .contaT (w_contaT),
-	  .timeout    (s_timeout),
-    .db_contagem    (s_contagem),
-    .db_jogada  (s_botoes),
-    .db_memoria (s_memoria),
-    .db_rodada  (s_rodada),
-    .jogada_feita   (w_jogada_feita),
-    .db_tem_jogada  (db_tem_jogada),
-    .leds   (leds),
-    .led_selector   (w_led_selector)    
+ 	exp6_fluxo_dados FD(
+    .clock                (clock_in),                     
+    .zeraCR               (zeraCR_in),   
+    .zeraE                (zeraE_in),    
+    .contaCR              (contaCR_in),    
+    .contaE               (contaE_in),   
+    .limpaRC              (limpaRC_in),    
+    .registraRC           (registraRC_in),   
+    .zeraLeds             (zeraLeds_in),   
+    .registraLeds         (registraLeds_in),   
+	  .contaT               (contaT_in),   
+    .botoes               (botoes_in),   
+    .led_selector         (led_selector_in),   
+    .jogada_correta       (jogada_correta_out),   
+    .enderecoIgualRodada  (enderecoIgualRodada_out),      
+    .fimC                 (fimC_out),   
+    .fimL                 (fimL_out),   
+    .jogada_feita         (jogada_feita_out),   
+    .db_tem_jogada        (db_tem_jogada_out),    
+	  .timeout              (timeout_out),    
+    .db_contagem          (db_contagem_out),    
+    .db_memoria           (db_memoria_out),   
+    .db_jogada            (db_jogada_out),    
+    .db_rodada            (db_rodada_out),    
+    .leds                 (leds_out)   
 );
 
     integer rodadaInt = 0;
@@ -95,72 +92,145 @@ wire [3:0] leds;
     initial begin
       $display("Inicio da simulacao");
 
-      // condicoes iniciais
-      caso       = 0;
-      clock_in   = 1;
-      reset_in   = 0;
-      jogar_in = 0;
-      botoes_in  = 4'b0000;
-      #clockPeriod;
+      // inicialização
+      jogar = 0;
+      clock_in = 1;
+      botoes_in = 4'd0;
+      Eatual <= idle;
+ 
+      //botões
+      botoes_in = 4'd0;
+      #(10*clockPeriod);
 
-      /*
-       * Cenario de Teste 1 - acerta tudo
-       */
+      // Começa o jogo => jogar = 1 => vai para o preparação
+      jogar = 1;
+      #(10*clockPeriod);
+      jogar = 0;
 
-      // Teste 1. resetar circuito
-      caso = 1;
-      // gera pulso de reset
-      @(negedge clock_in);
-      reset_in = 1;
+
+      // Início Rodada 1
+      #(10*clockPeriod);
+
+      // Espera
+      #(50*clockPeriod);
+
+      // Jogada
+      botoes_in = 0001;
       #(clockPeriod);
-      reset_in = 0;
-      // espera
-      #(10*clockPeriod);
 
-      // Teste 2. jogar=1 por 5 periodos de clock
-      // Começa o jogo
-      caso = 2;
-      jogar_in = 1;
-      #(5*clockPeriod);
-      jogar_in = 0;
-      // espera
-      #(10*clockPeriod);
+      // Registra
+      botoes_in = 0001;
+      #(clockPeriod);
+      
+      // Atualiza Memória
+      botoes_in = 0001;
+      #(clockPeriod);
+      
+      // Comparação
+      botoes_in = 0000;
+      #(clockPeriod);
 
+      // Ultima Jogada
+      botoes_in = 0000;
+      #(clockPeriod);
+      
+      // Proxima rodada
+      botoes_in = 0000;
+      #(clockPeriod);
 
-      for(rodadaInt = 0; rodadaInt <= 15; rodadaInt = rodadaInt + 1) begin
-        for(jogadaInt = 0; jogadaInt <= rodadaInt; jogadaInt = jogadaInt + 1) begin
-          caso = 3;
+      // rodada 2
 
-          case (jogadaInt)
-            4'b0000: botoes_in = 4'b0001;
-            4'b0001: botoes_in = 4'b0010;
-            4'b0010: botoes_in = 4'b0100;
-            4'b0011: botoes_in = 4'b1000;
-          endcase
+      // Início Rodada 2
+      botoes_in = 0000;
+      #(clockPeriod);
+      
+      // Proxima rodada
+      botoes_in = 0000;
+      #(clockPeriod);
 
-          if(rodadaInt == 4 && jogadaInt == 2) begin
-            botoes_in = 4'b0001;
-          end
+      // Espera
+      #(50*clockPeriod);
+      
+      // Jogada 1
+      botoes_in = 0001;
+      #(clockPeriod);
 
-          #(5*clockPeriod);
-          botoes_in = 4'b0000;
-          #(5*clockPeriod);
-        end
-      end
+      // Registra
+      botoes_in = 0001;
+      #(clockPeriod);
 
-      // Incia o jogo novamente
-      caso = 5;
-      @(negedge clock_in);
-      jogar_in = 4'b0001;
-      #(10*clockPeriod);
-      jogar_in = 4'b0000;
-      #(10*clockPeriod);
+      // Atualiza Memória 
+      #(clockPeriod);
 
-      // final dos casos de teste da simulacao
-      caso = 99;
+      // Comparação 
+      botoes_in = 0000;
+      #(clockPeriod);
+
+      // Proxima Jogada 
+      #(clockPeriod);
+
+      // Espera
+      #(50*clockPeriod);
+
+      // Jogada
+      botoes_in = 0001;
+      #(clockPeriod);
+
+      // Registra
+      botoes_in = 0001;
+      #(clockPeriod);
+
+      // Atualiza Memória 
+      #(clockPeriod);
+
+      // Comparação 
+      botoes_in = 0000;
+      #(clockPeriod);
+
+      // Perdeu
+       #(50*clockPeriod);
+
       #100;
       $display("Fim da simulacao");
       $stop;
     end
+
+      always @(posedge clock_in) begin
+            Eatual <= Eprox;
+      end
+        
+        always @* begin
+          
+        zeraCR_in        = (Eatual == idle || Eatual == preparacao) ? 1'b1 : 1'b0;
+        zeraE_in         = (Eatual == idle || Eatual == preparacao || Eatual == inicio) ? 1'b1 : 1'b0;
+        limpaRC_in       = (Eatual == idle || Eatual == preparacao) ? 1'b1 : 1'b0;
+        registraRC_in    = (Eatual == registra) ? 1'b1 : 1'b0;
+        zeraLeds_in      = (Eatual == idle || Eatual == preparacao) ? 1'b1 : 1'b0;
+        registraLeds_in  = (Eatual == registra || Eatual == inicio) ? 1'b1 : 1'b0;
+        contaCR_in       = (Eatual == proxima_rodada) ? 1'b1 : 1'b0;
+        contaE_in        = (Eatual == proxima_jogada) ? 1'b1 : 1'b0;
+	    	contaT_in        = (Eatual == espera) ? 1'b1 : 1'b0;
+        led_selector_in  = (Eatual == inicio || Eatual == preparacao || Eatual == proxima_rodada) ? 1'b1 : 1'b0;
+
+    end
+
+    always @* begin
+      case (Eatual)
+        idle:               Eprox = jogar ? preparacao : idle;
+        preparacao:         Eprox = inicio;
+        inicio:             Eprox = espera;
+        espera:             Eprox = timeout_out ? fim_T : (jogada_feita_out ? registra : espera);
+        registra:           Eprox = atualiza_memoria;
+        atualiza_memoria:   Eprox = comparacao;
+        comparacao:         Eprox = !jogada_correta_out ? fim_E : (enderecoIgualRodada_out ? ultima_jogada : proxima_jogada);
+        proxima_jogada:     Eprox = espera;
+        ultima_jogada:      Eprox = fimL_out ? fim_A : proxima_rodada;
+        proxima_rodada:     Eprox = inicio;
+        fim_T:              Eprox = jogar ? preparacao : fim_T;
+        fim_E:              Eprox = jogar ? preparacao : fim_E;
+        fim_A:              Eprox = jogar ? preparacao : fim_A;
+        default:            Eprox = idle;
+      endcase
+    end 
 
   endmodule
